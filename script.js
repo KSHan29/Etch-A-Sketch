b2a58829374d1a2ec0d2d2a7d56bf6294f6cbb0c
Kee Seow Han <seow.han@u.nus.edu>
2023-07-10 14:13:59 +0800

    Add UI buttons and slider for grid size

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 let gridSize = 10;
 const cellWidth = document.querySelector(".grid").clientWidth / 16;
+let colour = "blue";
+let backgroundColour = "white";
+
+function changeColour(e) {
+  //   e.target.style.backgroundColour = "black";
+  //   console.log(e.target);
+  e.target.style.background = colour;
+}
+
+function clearGrid() {
+  let cell = document.querySelector(".cell");
+  cell.style.background = backgroundColour;
+}
+
+function changeGridSize(e) {
+  let grid = document.querySelector(".grid");
+  grid.textContent = "";
+  gridSize = e.target.value;
+  let gridSizeDisplay = document.querySelector(".grid-size-display");
+  gridSizeDisplay.textContent = `Grid Size: ${gridSize}`;
+  init();
+  //   clearGrid();
+}
+
+function addListeners() {
+  let gridSizeControl = document.querySelector(".grid-size");
+  gridSizeControl.addEventListener("input", changeGridSize);
+}
 
 function init() {
   let grid = document.querySelector(".grid");
@@ -9,6 +37,7 @@ function init() {
     for (let j = 0; j < gridSize; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.addEventListener("mouseover", changeColour);
       row.appendChild(cell);
     }
     grid.appendChild(row);
@@ -16,3 +45,4 @@ function init() {
 }
 
 init();
+addListeners();
